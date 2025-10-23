b6acb78e903df8825d142f108473e79b85aff70a
Adwowa Montford <adwowamontford@gmail.com>
2025-10-23 09:07:16 +0100

    add questions to js

---
+++ assets/js/movie-script.js
@@ -0,0 +1,56 @@
+const quizData = [
+    {
+        question: "What is the most streamed film on Netflix?",
+        options: [
+            "K-Pop Demon Hunters",
+            "The Godfather",
+            "Bird Box",
+            "Glass Onion",
+        ],
+        correct: 1,
+    },
+    {
+        question: "Which is the highest grossing film of all time?",
+        options: [
+            "Avengers Infinity War",
+            "Avatar",
+            "Barbie",
+            "Star Wars: Episode I – The Phantom Menace",
+        ],
+        correct: 2,
+    },
+    {
+        question: "When did David Attenborough start presenting?",
+        options: ["1946", "1954", "1967", "1973"],
+        correct: 2,
+    },
+    {
+        question: "Who directed The Grand Budapest Hotel?",
+        options: [
+            "Wesley snipes",
+            "Wes Sanderson",
+            "Wendy Anderson",
+            "Wes Anderson",
+        ],
+        correct: 4,
+    },
+    {
+        question: "Where was Jaws primarily filmed?",
+        options: [
+            "Martha's Vineyard, Massachusetts",
+            "Bondi Beach, Australia",
+            "Miami Beach, Florida",
+            "Myrtle beach, South Carolina",
+        ],
+        correct: 1,
+    },
+];
+
+let currentQuestion = 0;
+let score = 0;
+
+const questionEl = document.getElementById("movie-question");
+const optionsEl = document.getElementById("movie-options");
+const nextBtn = document.getElementById("movie-nextBtn");
+const reset = document.getElementById("movie-resetBtn");
+const quizContainer = document.getElementById("movie-quiz-container");
